3ec8a355373a6feba05f1eadad3988b0706cf9ff
PawelManczak <pawel14522@gmail.com>
2023-03-12 19:20:28 +0100

    exercise 1.2

--- lab1/zadanie1.m
+++ lab1/zadanie1.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 a = 12;
 r_max=a/2;
-n_max = 400;
+n_max = 200;
 n = 0;
 
 x_h = [];
@@ -31,12 +31,11 @@ counter = 0;
         end
     end
 
-plot_circle(x, y, r);
 hold on;
 axis equal;
 axis([0 a 0 a]);
 
-while n < n_max
+while n < n_max -1
     n = n+1;
     counter = 0;
     while true
@@ -78,3 +77,25 @@ while n < n_max
     plot_circle(x, y, r);
     pause(0.01);
 end
+
+%exercise 1.1
+hold off;
+figure(2);
+
+
+plot(linspace(1, 200, 200), cumsum(fields));
+xlabel("Liczba narysowanych okręgów");
+ylabel("Powierzchnia");
+title("Powierzchnia całkowita kół");
+
+figure(3);
+second = cumsum(n_of_tries);
+
+
+for i = 1:200
+   second(i) = second(i)/i;
+end
+plot(linspace(1, 200, 200), second);
+xlabel("Liczba narysowanych okręgów");
+ylabel("Liczba losowań");
+title("Średnia liczba losowań");
