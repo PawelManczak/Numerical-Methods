0d04c3fd81ecd901200dbfd8ffd92e44da091c3b
PawelManczak <pawel14522@gmail.com>
2023-03-28 14:03:55 +0200

    exercise a,b

---
+++ lab2/main_lab3.m
@@ -0,0 +1,93 @@
+clc
+clear all
+close all
+
+% odpowiednie fragmenty kodu mozna wykonac poprzez zaznaczenie i wcisniecie F9 w Matlabie
+% komentowanie/odkomentowywanie: ctrl+r / ctrl+t
+
+% Zadanie A
+%------------------
+N = 10;
+density = 3; % parametr decydujacy o gestosci polaczen miedzy stronami
+[Edges] = generate_network(N, density);
+
+%-----------------
+
+% Zadanie B
+%------------------
+% generacja macierzy I, A, B i wektora b
+% macierze A, B i I musza byc przechowywane w formacie sparse (rzadkim)
+
+d = 0.85;
+B = sparse(Edges(2,:), Edges(1,:), 1, N, N);
+I = speye(N);
+L = sum(B);
+
+A = sparse(spdiags(1./L));
+
+M = sparse(I - d.*B.*A);
+b = ones(N,1);
+b(:,:) = (1 - d)/N;
+
+issparse(M)
+%-----------------
+
+
+
+% Zadanie D
+%------------------
+clc
+clear all
+close all
+
+N = [500, 1000, 3000, 6000, 12000];
+
+
+for i = 1:5
+    tic
+    % obliczenia start
+
+    % obliczenia stop
+    czas_Gauss(i) = toc;
+end
+plot(N, czas_Gauss)
+%------------------
+
+
+
+% Zadanie E
+%------------------
+clc
+clear all
+close all
+
+% sprawdz przykladowe dzialanie funkcji tril, triu, diag:
+% Z = rand(4,4)
+% tril(Z,-1) 
+% triu(Z,1) 
+% diag(diag(Z))
+
+
+for i = 1:5
+    tic
+    % obliczenia start
+
+    % obliczenia stop
+    czas_Jacobi(i) = toc;
+end
+%plot(N, czas_Jacobi)
+%------------------
+
+
+% Zadanie F
+%------------------
+
+
+% Zadanie G
+%------------------
+
+
+
+
+
+
