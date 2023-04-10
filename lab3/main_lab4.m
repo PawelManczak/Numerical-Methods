c2223fd1d9c443555bbee6d40bfc08120c9a57f6
PawelManczak <pawel14522@gmail.com>
2023-04-10 20:30:44 +0200

    ex1 bisection

--- lab3/main_lab4.m
+++ lab3/main_lab4.m
@@ -6,4 +6,17 @@ close all
 a = 0;   
 b = 50;
 
-[xvect, xdif, fx, it_cnt] = bisection(@compute_impedance,a,b,1e-12);
+% 1 bisection
+[xvect, xdif, fx, it_cnt] = bisection(@time,1,60000,10^-3);
+semilogy(1:it_cnt, xvect)
+title("1. Wartość kolejnego przybliżenia rozwiązania N w kolejnych iteracjach przy użyciu metody bisekcji");
+ylabel("Liczba parametrów wejściowych N");
+xlabel("Numer iteracji");
+saveas(gcf, 'ex1BisectionValues.png');
+
+figure(2);
+plot(1:it_cnt, xdif)
+title("1. Zmiany wartości przybliżonego rozwiązania N dla kolejnych przy użyciu metody bisekcji");
+ylabel("Różnica pomiędzy nową, a starą wartością N");
+xlabel("Numer iteracji");
+saveas(gcf, 'ex1BisectionDiff.png');
